888ffe82e56d60876d7c810d9ef38c7ee728ab72
lucasponez <lucasponez@outlook.com>
2020-09-15 21:23:49 +0000

    Revert "sanders: Build with temp sepolicy for now"
    This reverts commit c779e321beef2e279c4af694131a031b5f7aed33 and remove private folder of sepolicy.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -254,7 +254,8 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 VENDOR_SECURITY_PATCH := 2018-11-5
 
 # SELinux
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy-tmp
+include device/qcom/sepolicy-legacy-um/sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Thermal
 USE_DEVICE_SPECIFIC_THERMAL := true
